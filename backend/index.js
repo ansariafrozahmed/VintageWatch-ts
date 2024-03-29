fc38878575efb80c82bc23bca61deeb934ea9aa2
Ansari Afroz Ahmed <ansariafroz.py@gmail.com>
2024-03-29 18:23:57 +0530

    ALLOWD ORIGIN

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,10 @@ const PORT = 4000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://vintage-watch-ts-beta.vercel.app/",
+    ],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
